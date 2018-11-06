18e171799d10f1c896ce24c51dda735b7b694431
Rey <matsunagar97@gmail.com>
2018-11-06 15:49:26 -0800

    Initial Commit

---
+++ SpringsAndStructs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpringsAndStructs
+//
+//  Created by Rey Matsunaga on 11/6/18.
+//  Copyright © 2018 Rey Matsunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
